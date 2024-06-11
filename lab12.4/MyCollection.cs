1da2f2b0ea55a833c071fe9de86e096fbe4f9834
Тимофей Матвеев <matveev-timofey@mail.ru>
2024-06-11 17:46:16 +0300

    Добавьте файлы проекта.

---
+++ lab12.4/MyCollection.cs
@@ -0,0 +1,271 @@
+﻿using ClassLibraryLabor10;
+using labor121;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace lab12._4
+{
+    internal class MyCollection<T> : MyList<T>, IEnumerable<T>, IList<T> where T : IInit, ICloneable, new()
+    {
+        public MyCollection() : base() { }
+        public MyCollection(int size) : base(size) { count = size; }
+        public MyCollection(T[] collection) : base(collection) { }
+
+        private int count = 0;
+
+        public int Count => count;
+        public bool IsReadOnly => false;
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Count)
+                    throw new ArgumentOutOfRangeException(nameof(index));
+
+                Point<T> current = beg;
+                for (int i = 0; i < index; i++)
+                {
+                    current = current.Next;
+                }
+                return current.Data;
+            }
+            set
+            {
+                if (index < 0 || index >= Count)
+                    throw new ArgumentOutOfRangeException(nameof(index));
+
+                Point<T> current = beg;
+                for (int i = 0; i < index; i++)
+                {
+                    current = current.Next;
+                }
+                current.Data = value;
+            }
+        }
+
+        public void Add(T item)
+        {
+            AddToEnd(item);
+            count++;
+        }
+
+        public int IndexOf(T item)
+        {
+            Point<T> current = beg;
+            for (int i = 0; i < Count; i++)
+            {
+                if (current.Data.Equals(item))
+                    return i;
+                current = current.Next;
+            }
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index < 0 || index > Count)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            if (index == Count)
+            {
+                AddToEnd(item);
+                count++;
+                return;
+            }
+
+            if (index == 0)
+            {
+                Point<T> newItem = new Point<T>(item);
+                newItem.Next = beg;
+                if (beg != null)
+                {
+                    beg.Pred = newItem;
+                }
+                else
+                {
+                    end = newItem;
+                }
+                beg = newItem;
+                count++;
+                return;
+            }
+
+            Point<T> current = beg;
+            for (int i = 0; i < index; i++)
+            {
+                current = current.Next;
+            }
+
+            Point<T> newItemToAdd = new Point<T>(item);
+            newItemToAdd.Next = current;
+            newItemToAdd.Pred = current.Pred;
+
+            if (current.Pred != null)
+            {
+                current.Pred.Next = newItemToAdd;
+            }
+            else
+            {
+                beg = newItemToAdd;
+            }
+
+            current.Pred = newItemToAdd;
+            count++;
+        }
+
+        public void RemoveAt(int index)
+        {
+            Console.WriteLine($"Попытка удаления элемента с индексом {index}. Текущий размер коллекции: {count}");
+
+            if (index < 0 || index >= count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), "Индекс находится вне допустимого диапазона.");
+            }
+
+            Point<T> current = beg;
+            for (int i = 0; i < index; i++)
+            {
+                current = current.Next;
+            }
+
+            Console.WriteLine($"Удаление элемента: {current.Data}");
+
+            if (current.Pred != null)
+            {
+                current.Pred.Next = current.Next;
+            }
+            else
+            {
+                beg = current.Next;
+            }
+
+            if (current.Next != null)
+            {
+                current.Next.Pred = current.Pred;
+            }
+            else
+            {
+                end = current.Pred;
+            }
+
+            count--;
+            Console.WriteLine($"Элемент удалён. Новый размер коллекции: {count}");
+        }
+
+        public bool Contains(T item)
+        {
+            Point<T> current = beg;
+            while (current != null)
+            {
+                if (current.Data.Equals(item))
+                    return true;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array));
+
+            if (arrayIndex < 0)
+                throw new ArgumentOutOfRangeException(nameof(arrayIndex));
+
+            if (array.Length - arrayIndex < Count)
+                throw new ArgumentException("The number of elements in the source collection is greater than the available space from arrayIndex to the end of the destination array.");
+
+            Point<T> current = beg;
+            for (int i = arrayIndex; i < arrayIndex + Count; i++)
+            {
+                array[i] = current.Data;
+                current = current.Next;
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            Point<T> current = beg;
+            for (int i = 0; i < Count; i++)
+            {
+                if (current.Data.Equals(item))
+                {
+                    if (current.Pred != null)
+                    {
+                        current.Pred.Next = current.Next;
+                    }
+                    else
+                    {
+                        beg = current.Next;
+                    }
+
+                    if (current.Next != null)
+                    {
+                        current.Next.Pred = current.Pred;
+                    }
+                    else
+                    {
+                        end = current.Pred;
+                    }
+
+                    count--;
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new MyEnumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        internal class MyEnumerator : IEnumerator<T>
+        {
+            private Point<T> beg;
+            private Point<T> current;
+            private bool atStart;
+
+            public MyEnumerator(MyCollection<T> collection)
+            {
+                beg = collection.beg;
+                current = null;
+                atStart = true;
+            }
+
+            public T Current => current.Data;
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose() { }
+
+            public bool MoveNext()
+            {
+                if (atStart)
+                {
+                    current = beg;
+                    atStart = false;
+                }
+                else
+                {
+                    current = current?.Next;
+                }
+                return current != null;
+            }
+
+            public void Reset()
+            {
+                atStart = true;
+                current = null;
+            }
+        }
+    }
+}
---
+++ lab12.4/Program.cs
@@ -0,0 +1,284 @@
+﻿using ClassLibraryLabor10;
+using System;
+
+namespace lab12._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyCollection<Musicalinstrument> myCollection = null;
+
+            while (true)
+            {
+                Console.WriteLine("Меню:");
+                Console.WriteLine("1. Создать коллекцию со случайными элементами");
+                Console.WriteLine("2. Распечатать коллекцию");
+                Console.WriteLine("3. Добавить элемент в конец коллекции");
+                Console.WriteLine("4. Добавить элемент в начало коллекции");
+                Console.WriteLine("5. Удалить элемент из коллекции");
+                Console.WriteLine("6. Поиск элемента в коллекции");
+                Console.WriteLine("7. Очистить коллекцию");
+                Console.WriteLine("8. Добавить элемент (IList.Add)");
+                Console.WriteLine("9. Найти индекс элемента (IList.IndexOf)");
+                Console.WriteLine("10. Вставить элемент (IList.Insert)");
+                Console.WriteLine("11. Удалить элемент по индексу (IList.RemoveAt)");
+                Console.WriteLine("12. Проверить наличие элемента (ICollection<T>.Contains)");
+                Console.WriteLine("13. Скопировать элементы в массив (ICollection<T>.CopyTo)");
+                Console.WriteLine("14. Удалить элемент (ICollection<T>.Remove)");
+                Console.WriteLine("15. Выход");
+
+                if (!int.TryParse(Console.ReadLine(), out int answer))
+                {
+                    Console.WriteLine("Некорректный выбор. Попробуйте снова.");
+                    continue;
+                }
+
+                switch (answer)
+                {
+                    case 1:
+                        Console.WriteLine("Введите количество элементов:");
+                        if (int.TryParse(Console.ReadLine(), out int size))
+                        {
+                            myCollection = new MyCollection<Musicalinstrument>(size);
+                            Console.WriteLine("Коллекция создана.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Некорректный ввод.");
+                        }
+                        break;
+
+                    case 2:
+                        if (myCollection == null)
+                        {
+                            Console.WriteLine("Необходимо сначала создать коллекцию.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Распечатывание коллекции:");
+                            foreach (var item in myCollection)
+                            {
+                                Console.WriteLine(item);
+                            }
+                        }
+                        break;
+
+                    case 3:
+                        if (myCollection == null)
+                        {
+                            Console.WriteLine("Необходимо сначала создать коллекцию.");
+                        }
+                        else
+                        {
+                            Musicalinstrument newItem = new Musicalinstrument();
+                            newItem.Init();
+                            myCollection.AddToEnd(newItem);
+                            Console.WriteLine("Элемент добавлен в конец.");
+                        }
+                        break;
+
+                    case 4:
+                        if (myCollection == null)
+                        {
+                            Console.WriteLine("Необходимо сначала создать коллекцию.");
+                        }
+                        else
+                        {
+                            Musicalinstrument newItem = new Musicalinstrument();
+                            newItem.Init();
+                            myCollection.AddToBegin(newItem);
+                            Console.WriteLine("Элемент добавлен в начало.");
+                        }
+                        break;
+
+                    case 5:
+                        if (myCollection == null)
+                        {
+                            Console.WriteLine("Необходимо сначала создать коллекцию.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Введите индекс для удаления:");
+                            if (int.TryParse(Console.ReadLine(), out int index))
+                            {
+                                try
+                                {
+                                    myCollection.RemoveAt(index);
+                                    Console.WriteLine("Элемент удалён.");
+                                }
+                                catch (ArgumentOutOfRangeException ex)
+                                {
+                                    Console.WriteLine($"Ошибка: {ex.Message}");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Некорректный ввод.");
+                            }
+                        }
+                        break;
+
+                    case 6:
+                        if (myCollection == null)
+                        {
+                            Console.WriteLine("Необходимо сначала создать коллекцию.");
+                        }
+                        else
+                        {
+                            Musicalinstrument itemToFind = new Musicalinstrument();
+                            Console.WriteLine("Введите экземпляр для поиска:");
+                            itemToFind.Init();
+                            int foundIndex = myCollection.IndexOf(itemToFind);
+                            if (foundIndex != -1)
+                            {
+                                Console.WriteLine($"Экземпляр найден на индексе: {foundIndex}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Экземпляр не найден.");
+                            }
+                        }
+                        break;
+
+                    case 7:
+                        if (myCollection == null)
+                        {
+                            Console.WriteLine("Необходимо сначала создать коллекцию.");
+                        }
+                        else
+                        {
+                            myCollection = new MyCollection<Musicalinstrument>();
+                            Console.WriteLine("Коллекция очищена.");
+                        }
+                        break;
+
+                    case 8:
+                        if (myCollection == null)
+                        {
+                            Console.WriteLine("Необходимо сначала создать коллекцию.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Добавление элемента (IList.Add):");
+                            Musicalinstrument instrument1 = new Musicalinstrument();
+                            instrument1.Init();
+                            myCollection.Add(instrument1);
+                            Console.WriteLine("Элемент добавлен через Add.");
+                        }
+                        break;
+
+                    case 9:
+                        if (myCollection == null)
+                        {
+                            Console.WriteLine("Необходимо сначала создать коллекцию.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Поиск индекса элемента (IList.IndexOf):");
+                            Musicalinstrument instrument2 = new Musicalinstrument();
+                            instrument2.Init();
+                            int index = myCollection.IndexOf(instrument2);
+                            Console.WriteLine($"Индекс элемента: {index}");
+                        }
+                        break;
+
+                    case 10:
+                        if (myCollection == null)
+                        {
+                            Console.WriteLine("Необходимо сначала создать коллекцию.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Вставка элемента (IList.Insert):");
+                            Console.Write("Введите индекс для вставки: ");
+                            int insertIndex = int.Parse(Console.ReadLine());
+                            Musicalinstrument instrument3 = new Musicalinstrument();
+                            instrument3.Init();
+                            myCollection.Insert(insertIndex, instrument3);
+                            Console.WriteLine("Элемент вставлен через Insert.");
+                        }
+                        break;
+
+                    case 11:
+                        if (myCollection == null)
+                        {
+                            Console.WriteLine("Необходимо сначала создать коллекцию.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Удаление элемента по индексу (IList.RemoveAt):");
+                            Console.Write("Введите индекс для удаления: ");
+                            int removeIndex = int.Parse(Console.ReadLine());
+                            try
+                            {
+                                myCollection.RemoveAt(removeIndex);
+                                Console.WriteLine("Элемент удалён через RemoveAt.");
+                            }
+                            catch (ArgumentOutOfRangeException ex)
+                            {
+                                Console.WriteLine($"Ошибка: {ex.Message}");
+                            }
+                        }
+                        break;
+
+                    case 12:
+                        if (myCollection == null)
+                        {
+                            Console.WriteLine("Необходимо сначала создать коллекцию.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Проверка наличия элемента (ICollection<T>.Contains):");
+                            Musicalinstrument itemToCheck = new Musicalinstrument();
+                            itemToCheck.Init();
+                            bool contains = myCollection.Contains(itemToCheck);
+                            Console.WriteLine($"Элемент {(contains ? "найден" : "не найден")} в коллекции.");
+                        }
+                        break;
+
+                    case 13:
+                        if (myCollection == null)
+                        {
+                            Console.WriteLine("Необходимо сначала создать коллекцию.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Копирование элементов в массив (ICollection<T>.CopyTo):");
+                            Musicalinstrument[] array = new Musicalinstrument[myCollection.Count];
+                            myCollection.CopyTo(array, 0);
+                            Console.WriteLine("Элементы скопированы в массив:");
+                            foreach (var item in array)
+                            {
+                                Console.WriteLine(item);
+                            }
+                        }
+                        break;
+
+                    case 14:
+                        if (myCollection == null)
+                        {
+                            Console.WriteLine("Необходимо сначала создать коллекцию.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Удаление элемента (ICollection<T>.Remove):");
+                            Musicalinstrument itemToRemove = new Musicalinstrument();
+                            itemToRemove.Init();
+                            bool removed = myCollection.Remove(itemToRemove);
+                            Console.WriteLine($"Элемент {(removed ? "удален" : "не найден")} в коллекции.");
+                        }
+                        break;
+
+                    case 15:
+                        Console.WriteLine("Программа завершена.");
+                        return;
+
+                    default:
+                        Console.WriteLine("Некорректный выбор. Попробуйте снова.");
+                        break;
+                }
+            }
+        }
+    }
+}
